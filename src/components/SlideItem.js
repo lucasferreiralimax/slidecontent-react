6bd94569afae0d4f4a8d7a2f4d961b1c877e0ba1
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-02-04 16:15:13 -0300

    adjustment props className

--- src/components/SlideItem.js
+++ src/components/SlideItem.js
@@ -2,14 +2,8 @@ import React from 'react';
 import './SlideItem.css';
 
 function SlideItem(props) {
-  // const [actived, setActived] = useState(props.actived);
-  // let itemRef = useRef();
-  // useEffect(() => {
-  //   // console.log("itemRef");
-  //   // console.log(itemRef.current.classList);
-  // });
   return (
-    <article className={ "slide-item" + (props.actived ? " actived" : "" )}>
+    <article className={ `${ props.className } slide-item ${props.actived ? " actived" : ""} `}>
       <a href={props.url} target={props.target}>
         <h1>{props.title}</h1>
         <img src={props.img} alt={props.title} />
