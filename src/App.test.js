17ac37dd828a96a4ba14cfaf0f0040c479aab281
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-23 18:19:33 -0800

    test: unit basic App.js test titles and render slide-footer

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Should render <App/>", () => {
+  it('renders titles', () => {
+    render(<App />);
+    const titles = screen.getAllByText(/SlideContentReact/i);
+    const slideInfinity = screen.getByText(/Slide infinito/i);
+    const slideNavEmpty = screen.getByText(/Slide sem navegação/i);
+    const slideTime = screen.getByText(/Slide com duração de 10 segundos/i);
+
+    for(let title of titles) {
+      expect(title.textContent).toContain('SlideContentReact');
+    }
+
+    expect(slideInfinity).toBeInTheDocument()
+    expect(slideNavEmpty).toBeInTheDocument()
+    expect(slideTime).toBeInTheDocument()
+  });
+
+  it('renders slide-footer', () => {
+    render(<App />);
+    expect(screen.getByTestId('slide-footer')).toBeInTheDocument();
+  });
 });
