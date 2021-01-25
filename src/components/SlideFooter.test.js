262f2b6d71a9030ce27d635da03e222e1ea11199
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-25 15:42:07 -0800

    test: unit basic footer

---
+++ src/components/SlideFooter.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import SlideFooter from './SlideFooter';
+
+describe("Should render <SlideFooter/>", () => {
+  it('renders footer with credits and link', () => {
+    render(<SlideFooter/>);
+
+    const title = screen.getByTestId('slide-footer');
+    
+    expect(title.querySelector('a').href).toBe('https://github.com/lucasferreiralimax/slidecontent-react')
+    expect(title.querySelector('a').textContent).toBe('SlideContentReact - @lucasferreiralimax')
+  });
+});
