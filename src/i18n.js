1ecbef93dc6059975766e8e48d02ce3b4e9587d1
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-02-06 14:31:02 -0800

    adjustment fallback language

--- src/i18n.js
+++ src/i18n.js
@@ -8,8 +8,8 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
-    debug: true,
+    fallbackLng: 'pt-BR',
+    debug: false,
 
     interpolation: {
       escapeValue: false,
