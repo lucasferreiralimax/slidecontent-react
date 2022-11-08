a4c4768b4eaa54c6374412c543cf55f961065e78
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-08 15:27:09 -0300

    test(unit): add provider i18n in spec slideContent

--- src/components/SlideContent/test.spec.js
+++ src/components/SlideContent/test.spec.js
@@ -1,4 +1,7 @@
 import { render, screen } from '@testing-library/react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../../i18n';
+
 import SlideContent from '.';
 import SlideItem from '../SlideItem';
 import ermelino from './../../assets/ermelino.jpg';
@@ -7,10 +10,12 @@ import faveladascriancas from './../../assets/faveladascriancas.jpg';
 describe("Should render <SlideContent/>", () => {
   it('renders simple slide', () => {
     render(
-      <SlideContent>
-        <SlideItem title="Ermelino Matarazzo" img={ermelino} url="https://github.com/lucasferreiralimax/slidecontent-react" target="_blank" />
-        <SlideItem title="Fé em Deus" img={faveladascriancas} url="https://github.com/lucasferreiralimax/slidecontent-react" target="_blank" className="favela" />
-      </SlideContent>
+      <I18nextProvider i18n={i18n}>
+        <SlideContent>
+          <SlideItem title="Ermelino Matarazzo" img={ermelino} url="https://github.com/lucasferreiralimax/slidecontent-react" target="_blank" />
+          <SlideItem title="Fé em Deus" img={faveladascriancas} url="https://github.com/lucasferreiralimax/slidecontent-react" target="_blank" className="favela" />
+        </SlideContent>
+      </I18nextProvider>
     );
 
     const items = screen.getAllByTestId('link-item');
