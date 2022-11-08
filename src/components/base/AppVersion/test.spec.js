3fdba66209f13394eee87bd0a7163aa5c7b54728
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-08 16:54:56 -0300

    test(unit): add provider i18n in test appVersion

--- src/components/base/AppVersion/test.spec.js
+++ src/components/base/AppVersion/test.spec.js
@@ -1,9 +1,18 @@
 import { render, screen } from '@testing-library/react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../../../i18n';
+
 import AppVersion from '.';
 
 describe("Should render <AppVersion/>", () => {
-  it('renders appVersion', () => {
-    render(<AppVersion/>);
+  beforeEach(() => {
+    render(
+      <I18nextProvider i18n={i18n}>
+        <AppVersion />
+      </I18nextProvider>
+    );
+  })
+  it('renders AppVersion', () => {
     expect(screen.getByTestId('app-version')).toBeInTheDocument();
   });
 });
