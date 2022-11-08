3a535beb9b388a3bcaa8ae11e57c702517fd9160
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-08 16:49:58 -0300

    test(unit): adjustment footer validation text link with version

--- src/components/base/AppFooter/test.spec.js
+++ src/components/base/AppFooter/test.spec.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import AppFooter from '.';
 
+import pkg from '../../../../package.json';
+
 describe("Should render <AppFooter/>", () => {
   it('renders footer with credits and link', () => {
     render(<AppFooter/>);
@@ -8,6 +10,6 @@ describe("Should render <AppFooter/>", () => {
     const title = screen.getByTestId('app-footer');
 
     expect(title.querySelector('a').href).toBe('https://github.com/lucasferreiralimax/slidecontent-react')
-    expect(title.querySelector('a').textContent).toBe('SlideContentReact - @lucasferreiralimax')
+    expect(title.querySelector('a').textContent).toBe(`version ${pkg.version}SlideContentReact@lucasferreiralimax`)
   });
 });
