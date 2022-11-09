b76e8ed9c736fb018e4f64989204611c563b69fc
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-09 07:58:12 -0300

    add react-version in appVersion

--- src/components/base/AppVersion/index.js
+++ src/components/base/AppVersion/index.js
@@ -4,9 +4,15 @@ import pkg from '../../../../package.json';
 
 function AppVersion() {
   const appVersion = pkg.version;
+  const reactVersion = pkg.dependencies.react.replace('^', '');
   const { t } = useTranslation();
 
-  return <p className='app-version' data-testid='app-version'>{ t('version') } { appVersion }</p>;
+  return (
+    <>
+      <p className='react-version' data-testid='react-version'>{ t('version') } React { reactVersion }</p>
+      <p className='app-version' data-testid='app-version'>{ t('version') } SlideContent { appVersion }</p>
+    </>
+  );
 }
 
 export default AppVersion;
