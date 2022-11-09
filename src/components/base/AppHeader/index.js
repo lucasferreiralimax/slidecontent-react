999e7b896f4d787a7f0388ac546960ccc3a56d3f
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-09 07:58:00 -0300

    remove app version from Header

--- src/components/base/AppHeader/index.js
+++ src/components/base/AppHeader/index.js
@@ -3,14 +3,12 @@ import React from 'react';
 import AppLogo from '../AppLogo';
 import AppLanguage from '../AppLanguage';
 import AppNavigation from '../AppNavigation';
-import AppVersion from '../AppVersion';
 
 function AppHeader() {
   return (
     <header className="app-header" data-testid="app-header">
       <AppLanguage />
       <AppLogo />
-      <AppVersion />
       <AppNavigation />
     </header>
   );
